a5a04d76af1fe872c8586918ffcc0f21e59e580f
timonvandenbrink <timon.van.den.brink@gmail.com>
2016-06-13 21:59:41 +0200

    Added a test so jenkins would not complain anymore.

--- test/Main_TestAll.cpp
+++ test/Main_TestAll.cpp
@@ -1,6 +1,10 @@
 #include <limits.h>
 #include "gtest/gtest.h"
 
+TEST(Pilot, InitialTest){
+    ASSERT_TRUE(true);
+}
+
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
